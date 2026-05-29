2dfa28f399ddcd69d09485b5a74c8f9199e3a84e
Jeninson Peralta <jeninson@gmail.com>
2026-05-28 21:27:07 -0500

    Validando login con JS

--- js/main.js
+++ js/main.js
@@ -1,1 +1,23 @@
-//alert("Estamos listo con JavaScript")
+document.addEventListener('DOMContentLoaded', (e) => {
+    //alert('Hola, bienvenido a Gastosoft');
+});
+
+document.addEventListener('submit', (event) => {
+    if (event.target && event.target.id === 'loginForm') {
+        event.preventDefault(); // Evita el envío del formulario
+        login();
+    }
+});
+
+function login() {
+    console.log('Estamos en la función login()');
+    const usuario = document.getElementById('email').value;
+    const clave = document.querySelector('#clave').value;
+    let user = email.value;
+    console.log('Usuario:', usuario);
+    console.log('Contraseña:', clave);
+    console.log('Valor del campo email:', user);
+    alert(`Usuario: ${usuario}\nContraseña: ${clave}\nValor del campo email: ${user}`);
+}
+
+
