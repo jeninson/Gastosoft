97b81079706117ee3e4722225050926c4d85cea0
Jeninson Peralta <jeninson@gmail.com>
2026-06-04 21:11:03 -0500

    Aplicando fetch

--- js/login.js
+++ js/login.js
@@ -3,8 +3,6 @@ export function validarCredenciales() {
     const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const usuario = document.getElementById('email').value;
     const clave = document.querySelector('#clave').value;
-    console.log('Usuario:', usuario);
-    console.log('Contraseña:', clave);
     //Ahora debemos validar los datos
     if (!regexEmail.test(usuario)) {
         alert('Por favor, ingresa un correo electrónico válido.');
@@ -14,8 +12,30 @@ export function validarCredenciales() {
         alert('La contraseña debe tener al menos 4 caracteres.');
         return;
     }
-    const datos = {usuario, clave};
-    alert(`Usuario: ${usuario}\nContraseña: ${clave}`);
+    const datos = {parametros: {usuario, clave}, url:"http://localhost:8080/api/login", method: "POST"};
     console.log('Datos a enviar:', datos);
+    enviarPeticion(datos);
+}
+
+function enviarPeticion(datos) {
+    let {parametros, url, method} = datos;
+    try {
+        fetch(url, {
+            method,
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(parametros)
+        })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Respuesta del servidor:', data);
+        })
+        .catch(error => {
+            console.error('Error al enviar la petición:', error);
+        });
+    } catch (error) {
+        console.error('Error en la función enviarPeticion:', error);
+    }
     
 }
