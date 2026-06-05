0dcc58460088fe5416bbd762d24321839baa905e
Jeninson Peralta <jeninson@gmail.com>
2026-06-04 20:39:04 -0500

    Valicion del Login con Expresiones Regulares

--- js/login.js
+++ js/login.js
@@ -1,11 +1,21 @@
 export function validarCredenciales() {
     console.log('Estamos en la función validarCredenciales()');
+    const regexEmail = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
     const usuario = document.getElementById('email').value;
     const clave = document.querySelector('#clave').value;
-    let user = email.value;
     console.log('Usuario:', usuario);
     console.log('Contraseña:', clave);
-    console.log('Valor del campo email:', user);
-    alert(`Usuario: ${usuario}\nContraseña: ${clave}\nValor del campo email: ${user}`);
     //Ahora debemos validar los datos
+    if (!regexEmail.test(usuario)) {
+        alert('Por favor, ingresa un correo electrónico válido.');
+        return;
+    }
+    if (clave.length < 4) {
+        alert('La contraseña debe tener al menos 4 caracteres.');
+        return;
+    }
+    const datos = {usuario, clave};
+    alert(`Usuario: ${usuario}\nContraseña: ${clave}`);
+    console.log('Datos a enviar:', datos);
+    
 }
